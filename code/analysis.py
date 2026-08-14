59bc64f44b07fd7f4cf4378504988209aede974c
Henri Heyden <henri@heyden5.de>
2026-08-14 16:04:28 +0200

    Weiteres Aufräumen, Modularisierung, Wort Zählungen

--- code/analysis.py
+++ code/analysis.py
@@ -1,67 +1,12 @@
-import re
-import os
-import numpy as np
-import pandas as pd
-from tqdm.auto import tqdm
-from concurrent.futures import ThreadPoolExecutor
-
-# Configure tqdm to use pandas
-tqdm.pandas(desc="Processing")
-
-
-# Version of df.map which splits df into chunks for each core to take.
-def parallel_map(df, func, workers=None):
-    workers = workers or os.cpu_count()
-    print(f"USING {workers} WORKERS")
-    # split by integer positions so we can take Series slices (preserves .map)
-    indices = np.array_split(np.arange(len(df)), workers)
-
-    def apply_idx(idx):
-        return df.iloc[idx].progress_map(func)
-
-    with ThreadPoolExecutor(max_workers=workers) as pool:
-        results = list(pool.map(apply_idx, indices))
-
-    return pd.concat(results)
-
-
-def do(funcs, x):
-    for f in funcs:
-        f(x)
-
-
-def nothing(_):
-    pass
-
-
-word_counts = {}
-
-
-def count_words(line):
-    global word_counts
-    # Use a tokenizer to tokenize text.
-    for word in line:
-        # print(word)
-        if word not in word_counts:
-            word_counts[word] = 1
-        else:
-            word_counts[word] += 1
+from header import *
 
 
 def main():
     print("LOADING DATASET")
     df = pd.read_pickle("ndy_utf8_lemma.pkl")
-    print("LOADING DATASET COMPLETE")
 
-    # print(df)
-
-    print("STARTING MAP")
-
-    parallel_map(df["Text"], count_words, 1)  # 1 Worker because of collisions
-    # print(word_counts)
-    words = list(word_counts.keys())
-    words.sort(key=lambda x: -word_counts[x])
-    print(words[:50])
+    # print("STARTING MAP")
+    print(df)
 
 
 if __name__ == "__main__":
---
+++ code/gen_word_counts.py
@@ -0,0 +1,35 @@
+from header import *
+
+# Static global for latter use
+word_counts = {}
+
+
+def count_words(line):
+    global word_counts
+
+    # Line is an array of lemmatized words
+    for word in line:
+        # print(word)
+        if word not in word_counts:
+            word_counts[word] = 1
+        else:
+            word_counts[word] += 1
+
+
+def main():
+    print("LOADING DATASET")
+    df = pd.read_pickle("ndy_utf8_lemma.pkl")
+
+    print("STARTING MAP")
+
+    parallel_map(df["Text"], count_words, 1)  # 1 Worker because of collisions
+
+    print("SORTING")
+    wc_df = pd.DataFrame(word_counts.items(), columns=["Name", "Count"])
+    wc_df.sort_values(by="Count", ascending=False, inplace=True)
+
+    wc_df.to_pickle("ndy_utf8_word_counts.pkl")
+
+
+if __name__ == "__main__":
+    main()
